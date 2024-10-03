7c47adad5baa746ad41a577b1eb6ed226069e84f
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-10-04 03:28:07 +0600

    Update user type

--- src/types/user.ts
+++ src/types/user.ts
@@ -15,4 +15,32 @@ export type TTokenData = {
     roles: TUserRoles[];
     token: string;
     accountStatus: TAccountStatus;
+    exp: number
 }
+
+export type TBloodGroup =
+    | "A+"
+    | "A-"
+    | "B+"
+    | "B-"
+    | "O+"
+    | "O-"
+    | "AB+"
+    | "AB-";
+
+export type TGender = "male" | "female";
+
+export type TUser = {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    gender: TGender;
+    phone: string;
+    location: string;
+    accountStatus: TAccountStatus;
+    dateOfBirth: string;
+    bloodGroup: TBloodGroup;
+    profilePicture: string;
+};
