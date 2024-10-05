725aa27af47da5b8251671a40acff0b0391f804d
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-10-05 23:35:31 +0600

    Create layout for manage-pharmacy

---
+++ src/app/(privateLayout)/dashboard/admin/manage-pharmacy/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { TComponentProps } from "@/types";
+import Dashboard from "@/components/dashboard/Dashboard";
+import {Box} from "@mui/material";
+import PharmacyNavbar from "@/app/(privateLayout)/dashboard/admin/manage-pharmacy/_components/PharmacyNavbar";
+
+const Layout = ({ children }: TComponentProps) => {
+    return <>
+        <Box>
+            <PharmacyNavbar />
+            {children}
+        </Box>
+    </>
+};
+
+export default Layout;
