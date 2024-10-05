cef9a343164567df64cf7740287bb5bc8d72e68f
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-10-05 23:34:24 +0600

    Update tagTypes for rtk-query baseQuery

--- src/redux/tagTypes.ts
+++ src/redux/tagTypes.ts
@@ -1,3 +1,23 @@
-export enum tagTypes {}
+export enum tagTypes {
+  superAdmin = "superAdmin",
+  admin = "admin",
+  user = "user",
+  donor = "donor",
+  doctor = "doctor",
+  patient = "patient",
+  medicine = "medicine",
+  myself = "myself",
+  metaData = "metaData",
+}
 
-export const tagTypesList = [];
+export const tagTypesList = [
+  tagTypes.superAdmin,
+  tagTypes.admin,
+  tagTypes.user,
+  tagTypes.donor,
+  tagTypes.doctor,
+  tagTypes.patient,
+  tagTypes.medicine,
+  tagTypes.myself,
+  tagTypes.metaData
+];
