3ca0df93b21d9806aa338c86c4b4e91b528bfff5
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-10-05 23:31:57 +0600

    Create medicine type and update type index file

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export * from "./user";
 export * from "./componentProps";
 export * from "./dashboard";
+export * from "./medicine";
---
+++ src/types/medicine.ts
@@ -0,0 +1,17 @@
+export type TMedicine = {
+    id: string;
+    categoryName: string;
+    categoryId: string;
+    name: string;
+    description: string;
+    price: number;
+    brandName: string;
+    stockQuantity: number;
+    discount: number;
+    photo: string;
+};
+
+export type TMedicineCategory = {
+    id: string;
+    categoryName: string;
+};
