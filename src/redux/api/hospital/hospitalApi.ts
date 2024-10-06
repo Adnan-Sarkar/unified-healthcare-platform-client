19a465c7facd99d8b3687212ed3550ca020a5eb1
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-10-06 18:56:44 +0600

    Create hospital api using rtk-query

---
+++ src/redux/api/hospital/hospitalApi.ts
@@ -0,0 +1,43 @@
+import baseApi from "@/redux/api/baseApi";
+import { tagTypes } from "@/redux/tagTypes";
+import {THospital} from "@/types";
+
+const hospitalApi = baseApi.injectEndpoints({
+    endpoints: (builder) => ({
+        addNewHospital: builder.mutation({
+            query: (data) => ({
+                url: `/hospital/register-hospital`,
+                method: "POST",
+                data
+            }),
+            invalidatesTags: [tagTypes.hospital]
+        }),
+
+        getAllHospitals: builder.query({
+            query: (query: Record<string, any>) => ({
+                url: `/hospital/`,
+                method: "GET",
+                params: query
+            }),
+            providesTags: [tagTypes.hospital],
+        }),
+
+        updateHospitalInfo: builder.mutation({
+            query: ({updatedInfo, id}: {updatedInfo: THospital, id: string}) => ({
+                url: `/hospital/${id}`,
+                method: "PATCH",
+                data: updatedInfo
+            }),
+            invalidatesTags: [tagTypes.hospital],
+        }),
+
+
+
+    }),
+});
+
+export const {
+    useAddNewHospitalMutation,
+    useUpdateHospitalInfoMutation,
+    useGetAllHospitalsQuery
+} = hospitalApi;
